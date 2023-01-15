f720cbc5dd0295b2e0ed72a7396b55c362b98e33
MultiMilli <xwins2000m@gamil.com>
2023-01-15 17:04:48 +0200

    create db remote

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from peewee import SqliteDatabase, Model, CharField, DateTimeField, PostgresqlDatabase
 # import psycopg2
-from config import USERNAME, PASSWORD
+# from config import USERNAME, PASSWORD
 
 # try:
 #     conn = psycopg2.connect(
@@ -19,7 +19,7 @@ from config import USERNAME, PASSWORD
 
 # # db = SqliteDatabase('applicants') #Test bot on SQLite
 # db = PostgresqlDatabase('applicant', host='localhost', port=5432, user=USERNAME, password=PASSWORD)
-db = PostgresqlDatabase('db026qgkgqgq4i', host='ec2-63-35-156-160.eu-west-1.compute.amazonaws.com', user='ggldqqvsolhwlv', password='1e19c6271314697076783a70979f55628f33acd5cbe72bd0a9c6d0f816a73b8c')
+db = PostgresqlDatabase('db026qgkgqgq4i', host='ec2-63-35-156-160.eu-west-1.compute.amazonaws.com', user='ggldqqvsolhwlv', password='1e19c6271314697076783a70979f55628f33acd5cbe72bd0a9c6d0f816a73b8c', port=5432)
 
 class User(Model):
     chat_id = CharField()
@@ -31,7 +31,7 @@ class User(Model):
         database = db
 
 if __name__ == '__main__':
-    # db.connect()
+    db.connect()
     db.create_tables([User])
 
 
